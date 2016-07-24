59a4c0222a33dba45c3196f746ea1e100c0bbb98
Uyen Mai <Uyen Mai>
2016-07-24 16:11:40 -0700

    a working column alignment

--- core_splitting_lib.py
+++ core_splitting_lib.py
@@ -8,7 +8,7 @@ MAX_INTENSITY = 255
 NOISE_TORL_RATIO = 0.01 # noise torlerance ratio: maximum proportion of the pixels in the splitting line that are noise (noises that was failed to be dithered out)
 NOISE_TORL = MAX_INTENSITY*NOISE_TORL_RATIO
 MIN_TO_TPC = 0.5 # the minimum ratio of an obj to the "typical" in an objectList
-OFFSET_RATIO = 0.5 # the maximum offset of objects in a column/row (relative to object widh/height)
+OFFSET_RATIO = 0.3 # the maximum offset of objects in a column/row (relative to object widh/height)
 
 def naive_dithering(img):
 	# input: an image
@@ -120,7 +120,7 @@ def find_all_splits(dither_img,objs,x_start,x_end,y_start,y_end):
 # a rectangle is located by 4 coordinates: x_start, x_end, y_start, y_end
 # therefore, each element in the objList passed to this function has the form 
 # [x_start, x_end, y_start, y_end]
-def typical_bounding_area(objList,method='avg'):
+def typical_obj_size(objList,method='avg'):
     # assume a majority of objects in objList are similar in size
     # this function return the area of a "typical" object in the list
     # method: 
@@ -132,11 +132,11 @@ def typical_bounding_area(objList,method='avg'):
     if method == 'med':
         tpc_width = np.median([abs(obj[1]-obj[0]) for obj in objList])
         tpc_height = np.median([abs(obj[3]-obj[2]) for obj in objList])
-        return tpc_width*tpc_height
+        return [tpc_width,tpc_height]
     else:
         tpc_width = np.mean([abs(obj[1]-obj[0]) for obj in objList])
         tpc_height = np.mean([abs(obj[3]-obj[2]) for obj in objList])
-        return tpc_width*tpc_height
+        return [tpc_width,tpc_height]
         
 def obj_area(obj):
     return abs((obj[3]-obj[2])*(obj[1]-obj[0]))
@@ -144,43 +144,59 @@ def obj_area(obj):
 def remove_tiny_objs(objList):
     # the splitting algorithm produces some tiny pieces that are not real objects
     # this function remove those from the objList
-    tpc_area = typical_bounding_area(objList)
+    tpc_size = typical_obj_size(objList)
+    tpc_area = tpc_size[0]*tpc_size[1]
     return [obj for obj in objList if obj_area(obj)/tpc_area >= MIN_TO_TPC]
  
 def find_centers(objList):
     return [[(obj[1]+obj[0])/2,(obj[3]+obj[2])/2] for obj in objList]
 
-def organize_obj(objList):
+def group2rowNsort(objList):
     # sort objects by y-coordinate and x-coordinate
     # after sorting, the objects can be read from left-> right and top->bottom
 
     # find object's center    
     c = find_centers(objList)
-    THRES = OFFSET_RATIO*np.mean([abs(obj[3]-obj[2]) for obj in objList])
+    THRES = OFFSET_RATIO*np.median([abs(obj[3]-obj[2]) for obj in objList])
     #THRES = OFFSET_RATIO*imgH
 
     # sort by vertical dimension
     sIdx = np.argsort([ctr[1] for ctr in c])
     
     # after sorting, objects in the same row are clusterred together
-    # traverse the sorted list to split the clusters (in this stage: cluster = row)
+    # traverse the sorted list to split the clusters (in this stage: cluster means row)
     # then sort objects in each cluster by column
-    organized_list = []
+    rowList = []
     i = 0    
     for j in range(1,len(sIdx)):
         if abs(c[sIdx[j]][1]-c[sIdx[j-1]][1]) > THRES:
             # sort by x-coordinate for objects within a row
             sIdx_row = sIdx[i:j]
             sortRow_idx = np.argsort([c[k][0] for k in sIdx_row])
-            organized_list.append([objList[sIdx_row[k]] for k in sortRow_idx])
+            rowList.append([objList[sIdx_row[k]] for k in sortRow_idx])
             i = j
             
     # add the last row
     sIdx_row = sIdx[i:]
     sortRow_idx = np.argsort([c[k][0] for k in sIdx_row])
-    organized_list.append([objList[sIdx_row[k]] for k in sortRow_idx])
+    rowList.append([objList[sIdx_row[k]] for k in sortRow_idx])
  
-    return organized_list
+    return rowList
+
+def column_alignment(rowList):
+    j = 0
+    while j< max([len(row) for row in rowList]):
+        col = [row[j] for row in rowList]
+        THRES = np.median([abs(obj[1]-obj[0]) for obj in col])
+        col_pos = np.min([(obj[1]+obj[0])/2 for obj in col])
+        i = 0
+        for obj in col:
+            if abs((obj[1]+obj[0])/2-col_pos) > THRES:
+                # add a virtual object
+                print i, j
+                rowList[i].insert(j,None)
+            i = i+1
+        j = j+1
             
 def show_objs_by_row(orgList,image):
     colors = [(255,0,0),(0,255,0),(0,0,255)]
@@ -193,6 +209,22 @@ def show_objs_by_row(orgList,image):
         		y_end = obj[3]
         		cv2.rectangle(image,(x_start,y_start),(x_end,y_end),colors[i%len(colors)],2)
         i = i+1
+    # illustration
+    cv2.imshow("split lines",image)
+    cv2.waitKey(0)
+    
+def show_objs_by_column(alignedList,image):
+    colors = [(255,0,0),(0,255,0),(0,0,255)]
+    i = 0
+    
+    for row in alignedList:
+        for obj in row:
+            x_start = obj[0]
+            x_end = obj[1]
+            y_start = obj[2]
+            y_end = obj[3]
+            cv2.rectangle(image,(x_start,y_start),(x_end,y_end),colors[i%len(colors)],2)
+            i = i+1
     # illustration
     cv2.imshow("split lines",image)
     cv2.waitKey(0)
